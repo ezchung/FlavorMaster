234612127681b52f24ba64900e3c47b5f3c339ae
Ezra Chung <ezra.chung97@gmail.com>
2024-07-19 12:26:49 -0700

    serializers for User

---
+++ FMBackend/classes/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Class
+
+#creating serailizers that takes User object into convertable code (JSON)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwangs = {"password":{"write-only":True}}
+
+    '''Takes data that is already validated from class Meta fields
+        Then creates new User
+    '''
+    def create(self, validated_data):
+        # print(validated_data)
+        user = User.objects.create_user(**validated_data)
+        return user 
